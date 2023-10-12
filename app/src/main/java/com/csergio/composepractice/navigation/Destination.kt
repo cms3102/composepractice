a0b56ff361233b7f6c9f46a82829cb72fc31864c
CMS <ayd07@naver.com>
2023-10-12 23:29:44 +0900

    dialog

--- app/src/main/java/com/csergio/composepractice/navigation/Destination.kt
+++ app/src/main/java/com/csergio/composepractice/navigation/Destination.kt
@@ -1,9 +1,25 @@
 package com.csergio.composepractice.navigation
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.outlined.AddCircle
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.csergio.common.protocol.DestinationProtocol
 import com.csergio.composepractice.R
 import com.csergio.settings.navigation.Settings
@@ -30,6 +46,29 @@ object Undefined : DestinationProtocol {
     override val appBarTitle: Int = R.string.undefined_app_bar_title
     override val route: String = "undefined_route"
     override val isMain: Boolean = true
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun undefinedMessageDialog(onDismissRequest: () -> Unit) {
+    AlertDialog(onDismissRequest = { onDismissRequest() }) {
+        Card(
+            modifier = Modifier
+                .size(100.dp),
+            shape = RoundedCornerShape(8.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.White)
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = stringResource(id = R.string.undefined_snackbar_message),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
 }
 
 
--- app/src/main/java/com/csergio/composepractice/ui/MyApp.kt
+++ app/src/main/java/com/csergio/composepractice/ui/MyApp.kt
@@ -1,9 +1,16 @@
 package com.csergio.composepractice.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -14,13 +21,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
@@ -36,6 +47,7 @@ import com.csergio.composepractice.navigation.NavigationHelper.findDestination
 import com.csergio.composepractice.navigation.NavigationHelper.navigateToTab
 import com.csergio.composepractice.navigation.Undefined
 import com.csergio.composepractice.navigation.mainDestinations
+import com.csergio.composepractice.navigation.undefinedMessageDialog
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlin.coroutines.coroutineContext
@@ -44,8 +56,15 @@ import kotlin.coroutines.coroutineContext
 @Composable
 fun MyApp(navHostController: NavHostController = rememberNavController()) {
 
+    var dialogState by remember { mutableStateOf(false) }
     val snackbarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
+
+    if (dialogState) {
+        undefinedMessageDialog {
+            dialogState = false
+        }
+    }
 
     MyScaffold(
         navController = navHostController,
@@ -62,9 +81,10 @@ fun MyApp(navHostController: NavHostController = rememberNavController()) {
                     currentDestination = currentDestination,
                 ) { destination ->
                     if (destination.route == Undefined.route) {
-                        coroutineScope.launch {
-                            snackbarHostState.showSnackbar(message = undefinedMessage)
-                        }
+                        dialogState = true
+//                        coroutineScope.launch {
+//                            snackbarHostState.showSnackbar(message = undefinedMessage)
+//                        }
                     } else {
                         navHostController.navigateToTab(destination)
                     }
